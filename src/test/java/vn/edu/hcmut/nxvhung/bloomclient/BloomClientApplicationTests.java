233fd311e0602a668c9b8fc6f06071a793081d89
Hung Nguyen <hung.nguyen04@hastech.vn>
2023-07-11 23:21:35 +0700

    initial commit

---
+++ src/test/java/vn/edu/hcmut/nxvhung/bloomclient/BloomClientApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmut.nxvhung.bloomclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
