ed701d35a5ce8196adf53046f8d9ee6b05d85f0e
Hung Nguyen <hung.nguyen04@hastech.vn>
2023-08-23 23:15:21 +0700

    enabled scheduling

--- src/main/java/vn/edu/hcmut/nxvhung/bloomclient/BloomClientApplication.java
+++ src/main/java/vn/edu/hcmut/nxvhung/bloomclient/BloomClientApplication.java
@@ -3,9 +3,11 @@ package vn.edu.hcmut.nxvhung.bloomclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class BloomClientApplication {
 
 	public static void main(String[] args) {
